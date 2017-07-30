9e98021fec7585bbe8b388f7c66f2ea92dba2dfb
andrew_kandzuba <andrey.kandzuba@gmail.com>
2017-07-30 16:04:50 -0500

    complete puzzle

--- puzzles/payments.go
+++ puzzles/payments.go
@@ -1,60 +1,79 @@
 package puzzles
 
-import "fmt"
+/**
+Puzzle
 
-func findComprise(incoming []int, target int) []int {
-	comprised := make([]int, 0)
+At a payments processing company, optimizing incoming and outgoing payments is a type of fee optimization.
 
-	fmt.Println(incoming)
+Below, we have a list of the incoming payments that we optimized as outgoing payments.
 
-	for i, v := range incoming {
+The outgoing payments are lump sums comprised of the some of the incoming payments.
+
+Write an algorithm that will show for each outgoing amount which incoming amounts it is comprised of.
+
+Identify any potential issues with your solution.
+
+Incoming amounts:
+
+100 100 225 300 473 80
+
+Outgoing amounts:
+
+180 773 225 100
+
+Solution:
+===
+
+The algorithm is trivial:
+
+1) Scan the list of incoming payments having the target amount.
+2) If the element is greater than the amount just skip it.
+2) If the element is equals to the amount it means the chain of incoming payments has been found.
+3) If element is less than the amount remember it, deduct it from the amount then recursively apply same
+   steps to the list excluding the element and having new amount. Merge result upon return.
+
+Issues:
+
+- Complexity: O(n2). It can be improved using BTS: no need to continue with scan if (v > target) => O(n x n x logN)
+- The algorithm does not return all possible chains. Introduce special structure (Go) or class (Java) at next steps:
+
+struct Payment {
+	id int
+	amount int
+}
+
+and return all possible combinations i.e. ([1:100],[6:80]),([2:100],[6:80])
+
+
+*/
+func find(a []int, target int) []int {
+
+	comprise := make([]int, 0)
+
+	if len(a) == 0 || target == 0 {
+		return comprise
+	}
+
+	for i, v := range a {
 
 		if v > target {
 			continue
 		}
 
 		if v == target {
-			comprised = append(comprised, v)
-			continue
+			return append(comprise, v)
 		}
 
-		c := findComprise(incoming[i+1:], target-v)
+		c := find(a[i+1:], target-v)
 
 		if len(c) > 0 {
-			for _, cv:= range c {
-				comprised = append(comprised, cv)
+			comprise = append(comprise, v)
+			for _, cv := range c {
+				comprise = append(comprise, cv)
 			}
+			break
 		}
 	}
 
-	return comprised
-}
-
-func find(index int, incoming []int, comprised []int, target int) []int {
-	if index < 0 || index == len(incoming) {
-		if target == 0 {
-			return comprised
-		} else {
-			return make([]int, 0)
-		}
-	}
-
-	c, t := NewTarget(comprised, target, incoming[index]);
-	lf := find(0, incoming[0:index-1], c, t)
-	rf := find(index-1, incoming, c, t)
-
-	if len(lf) > 0 {
-		return lf
-	}
-	return rf
-}
-
-func NewTarget(comprised []int, target int, amount int) ([]int, int) {
-	deduct := target - amount
-
-	if deduct >= 0 {
-		return append(comprised, amount), deduct
-	}
-
-	return comprised, target;
+	return comprise
 }
--- puzzles/payments_test.go
+++ puzzles/payments_test.go
@@ -2,39 +2,19 @@ package puzzles
 
 import (
 	"testing"
-	//"github.com/stretchr/testify/assert"
-	"fmt"
+	"github.com/stretchr/testify/assert"
 )
 
+
 func TestFind(t *testing.T) {
-	/**
-Puzzle
-
-At a payments processing company, optimizing incoming and outgoing payments is a type of fee optimization.
-
-Below, we have a list of the incoming payments that we optimized as outgoing payments.
-
-The outgoing payments are lump sums comprised of the some of the incoming payments.
-
-Write an algorithm that will show for each outgoing amount which incoming amounts it is comprised of.
-
-Identify any potential issues with your solution.
-
-Incoming amounts:
-
-100 100 225 300 473 80
-
-Outgoing amounts:
-
-180 773 225 100
-	 */
 
 	incoming := []int{100, 100, 225, 300, 473, 80}
-	//outgoing := []int{180, 773, 225, 100}
-	outgoing := []int{180}
 
-	for _, amount := range outgoing {
-		comprised := findComprise(incoming, amount)
-		fmt.Println(comprised)
-	}
+	assert.Equal(t, find(incoming, 180), []int{100, 80})
+	assert.Equal(t, find(incoming, 773), []int{300, 473})
+	assert.Equal(t, find(incoming, 225), []int{225})
+	assert.Equal(t, find(incoming, 325), []int{100, 225})
+	assert.Equal(t, find(incoming, 335), []int{})
+	assert.Equal(t, find(incoming, 573), []int{100, 473})
 }
+

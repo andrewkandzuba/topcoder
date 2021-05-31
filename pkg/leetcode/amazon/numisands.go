9fb79c6297b5c29b89e2e167ef864c42b968ba61
andrew_kandzuba <andrey.kandzuba@gmail.com>
2021-05-31 11:23:40 -0500

    number of islands - breadth search first

---
+++ pkg/leetcode/amazon/numisands.go
@@ -0,0 +1,48 @@
+package amazon
+
+func numIslands(grid [][]byte) int {
+	islands := 0
+	for x := 0; x < len(grid); x++ {
+		for y := 0; y < len(grid); y++ {
+			if grid[x][y] == 1 {
+				islands++
+				search(grid, &GridNode{x, y})
+			}
+		}
+	}
+	return islands
+}
+
+func search(grid [][]byte, root *GridNode) {
+	queue := make([]*GridNode, 0)
+	grid[root.x][root.y] = 0 // mark as visited
+
+	queue = append(queue, root)
+	for len(queue) > 0 {
+		r := queue[0]
+		queue = queue[1:]
+
+		if r.x -1 >= 0 && grid[r.x-1][r.y] != 0 {
+			grid[r.x-1][r.y] = 0
+			queue = append(queue, &GridNode{r.x-1, r.y})
+		}
+		if r.x + 1 < len(grid) && grid[r.x+1][r.y] != 0 {
+			grid[r.x+1][r.y] = 0
+			queue = append(queue, &GridNode{r.x + 1, r.y})
+		}
+		if r.y - 1 >= 0 && grid[r.x][r.y-1] != 0 {
+			grid[r.x][r.y-1] = 0
+			queue = append(queue, &GridNode{r.x, r.y-1})
+		}
+		if r.y + 1 < len(grid[0]) && grid[r.x][r.y+1] != 0 {
+			grid[r.x][r.y+1] = 0
+			queue = append(queue, &GridNode{r.x, r.y+1})
+		}
+
+	}
+}
+
+type GridNode struct {
+	x int
+	y int
+}
---
+++ pkg/leetcode/amazon/numisands_test.go
@@ -0,0 +1,57 @@
+package amazon
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func Test_NumIslands_Case1(t *testing.T) {
+	assert.EqualValues(t, 1, numIslands([][]byte{
+		{1, 1, 1, 1, 0},
+		{1, 1, 0, 1, 0},
+		{1, 1, 0, 0, 0},
+		{0, 0, 0, 0, 0},
+	}))
+}
+
+func Test_NumIslands_Case2(t *testing.T) {
+	assert.EqualValues(t, 3, numIslands([][]byte{
+		{1, 1, 0, 0, 0},
+		{1, 1, 0, 0, 0},
+		{0, 0, 1, 0, 0},
+		{0, 0, 0, 1, 1},
+	}))
+}
+
+func Test_NumIslands_Case3(t *testing.T) {
+	assert.EqualValues(t, 2, numIslands([][]byte{
+		{1, 1, 0, 0, 0},
+		{1, 1, 0, 0, 0},
+		{0, 0, 0, 0, 0},
+		{0, 0, 0, 1, 1},
+	}))
+}
+
+func Test_NumIslands_Case4(t *testing.T) {
+	assert.EqualValues(t, 1, numIslands([][]byte{
+		{0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0},
+		{0, 0, 1, 0, 0},
+		{0, 0, 0, 0, 0},
+	}))
+}
+
+func Test_NumIslands_Case5(t *testing.T) {
+	assert.EqualValues(t, 1, numIslands([][]byte{
+		{1, 1, 1, 1, 1},
+		{1, 0, 0, 0, 1},
+		{1, 0, 0, 0, 1},
+		{1, 1, 1, 1, 1},
+	}))
+}
+
+func Test_NumIslands_Case6(t *testing.T) {
+	assert.EqualValues(t, 1, numIslands([][]byte{
+		{1},
+	}))
+}
